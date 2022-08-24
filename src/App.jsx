11177e12537d950ef7f2734697271d934d79c2d4
Gonzalo Nozikovsky <nozi22xt@gmail.com>
2022-08-24 10:56:07 -0300

    subo singleproduct view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import SingleProductoView from "./components/SingleProduct";
 
 function App() {
   return (
-    <div></div>
+    <>
+      <Navbar />
+      <SingleProductoView />
+    </>
   );
 }
 
