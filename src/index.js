15ab167f95268c8a52efa3d165d05d414fbba9f8
Gonzalo Nozikovsky <nozi22xt@gmail.com>
2022-08-24 18:18:38 -0300

    configuracion redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-//import { Provider } from 'react-redux';
-//import { store } from './app/store';
 import App from "./App";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import cartReducer from "./cart";
 
-// export const store = configureStore({
-//   reducer: {
-//     counter: counterReducer,
-//   },
-// });
+export const store = configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {
+    cart: cartReducer,
+  },
+});
