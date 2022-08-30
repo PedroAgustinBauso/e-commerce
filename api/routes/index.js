5cba8f339da5366bdea664428492c53f7f2454e7
emosich <erikmosich@gmail.com>
2022-08-30 19:04:17 -0300

    ruta past orders

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const products = require("./products");
 const cartRouter = require("./cart");
 const category = require("./category");
+const pastOrders = require("./pastOrders");
 
 router.use("/cart", cartRouter);
 router.use("/products", products);
 router.use("/users", user);
 router.use("/category", category);
+router.use("/pastOrders", pastOrders);
 
 module.exports = router;
