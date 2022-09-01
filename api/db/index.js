00ac45a64f8f0b74228d662aec36ae02b312bd36
Franco Jalil <francojalil7@gmail.com>
2022-09-01 18:41:09 -0300

    carrito de jueves

--- api/db/index.js
+++ api/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("bbt", "gonzalo", null, {
+const sequelize = new Sequelize("BBT", null, null, {
   host: "localhost",
   dialect: "postgres",
   logging: false,
